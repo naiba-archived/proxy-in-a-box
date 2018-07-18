fa205e2059014453ff59279e21a424b3773f35cf
Naiba <1@5.nu>
2018-07-18 12:25:04 +0800

    cobra cli

--- cmd/proxy-in-a-box/main.go
+++ cmd/proxy-in-a-box/main.go
@@ -2,31 +2,52 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"strconv"
 
 	"github.com/robfig/cron"
+	"github.com/spf13/cobra"
 
 	"github.com/naiba/proxyinabox"
 	"github.com/naiba/proxyinabox/crawler"
 	"github.com/naiba/proxyinabox/proxy"
 )
 
+var configFilePath, httpProxyPort, httpsProxyPort string
+var rootCmd = &cobra.Command{
+	Use:   "proxy-in-a-box",
+	Short: "Proxy-in-a-Box provide many proxies.",
+	Long:  `Proxy-in-a-Box helps programmers quickly and easily develop powerful crawler services. one-script, easy-to-use: proxies in a box.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("AppName:", proxyinabox.AppName)
+		fmt.Println("AppVersion:", proxyinabox.AppVersion)
+
+		crawler.FetchProxies()
+		//crawler.Verify()
+
+		c := cron.New()
+		c.AddFunc("@daily", crawler.FetchProxies)
+		c.AddFunc("0 "+strconv.Itoa(proxyinabox.VerifyDuration)+" * * * *", crawler.Verify)
+		c.Start()
+
+		proxy.Serv(httpProxyPort, httpsProxyPort)
+
+		fmt.Println("HTTP proxy: `http://localhost:" + httpProxyPort + "`")
+		fmt.Println("HTTPS proxy: `https://localhost:" + httpsProxyPort + "`")
+
+		select {}
+	},
+}
+
+func init() {
+	rootCmd.PersistentFlags().StringVarP(&configFilePath, "conf", "c", "./pb.yaml", "config file")
+	rootCmd.PersistentFlags().StringVarP(&httpProxyPort, "hp", "p", "8080", "http proxy server port")
+	rootCmd.PersistentFlags().StringVarP(&httpProxyPort, "sp", "s", "8081", "https proxy server port")
+}
+
 func main() {
-	fmt.Println("AppName:", proxyinabox.AppName)
-	fmt.Println("AppVersion:", proxyinabox.AppVersion)
-
-	crawler.FetchProxies()
-	//crawler.Verify()
-
-	c := cron.New()
-	c.AddFunc("@daily", crawler.FetchProxies)
-	c.AddFunc("0 "+strconv.Itoa(proxyinabox.VerifyDuration)+" * * * *", crawler.Verify)
-	c.Start()
-
-	proxy.Serv("8080", "8081")
-
-	fmt.Println("HTTP proxy: `http://localhost:8080`")
-	fmt.Println("HTTPS proxy: `https://localhost:8081`")
-
-	select {}
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
