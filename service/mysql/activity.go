0b44c8ef7416ed0e68cf33043247d2f15386c864
Naiba <1@5.nu>
2018-07-18 11:59:37 +0800

    mysql optimized

--- service/mysql/activity.go
+++ service/mysql/activity.go
@@ -20,7 +20,7 @@ func (as *ActivityService) GetByDomainID(did uint) (a []proxyinabox.Activity, e 
 func (as *ActivityService) Save(d, p uint) {
 	// save avtivity
 	var a proxyinabox.Activity
-	if e := as.DB.First(&a, "domain_id = ? AND proxy_id = ?", d, p).Error; e == nil {
+	if e := as.DB.Select("id,usenum").First(&a, "domain_id = ? AND proxy_id = ?", d, p).Error; e == nil {
 		a.Usenum++
 		as.DB.Model(&a).Update("usenum", a.Usenum)
 	} else if e == gorm.ErrRecordNotFound {
@@ -32,7 +32,7 @@ func (as *ActivityService) Save(d, p uint) {
 	}
 	// update proxy use
 	var proxy proxyinabox.Proxy
-	if as.DB.First(&proxy, p).Error == nil {
+	if as.DB.Select("id,usenum").First(&proxy, p).Error == nil {
 		proxy.Usenum++
 		as.DB.Model(&proxy).Update("usenum", proxy.Usenum)
 	}
--- service/mysql/proxy.go
+++ service/mysql/proxy.go
@@ -15,17 +15,17 @@ type ProxyService struct {
 //GetByIP get proxy by ip
 func (ps *ProxyService) GetByIP(ip string) (proxyinabox.Proxy, error) {
 	var p proxyinabox.Proxy
-	return p, ps.DB.First(&p, "ip = ?", ip).Error
+	return p, ps.DB.Select("ip,port,id").First(&p, "ip = ?", ip).Error
 }
 
 //GetFree get a free proxy
 func (ps *ProxyService) GetFree(notIn []uint) (p proxyinabox.Proxy, e error) {
-	e = ps.DB.Not(notIn).Order("usenum ASC,delay ASC").First(&p).Error
+	e = ps.DB.Select("ip,port,id,usenum,delay").Not(notIn).Order("usenum ASC,delay ASC").First(&p).Error
 	return
 }
 
 //GetUnVerified get un verified proxies
 func (ps *ProxyService) GetUnVerified() (p []proxyinabox.Proxy, e error) {
-	e = ps.DB.Where("last_verify < ?", time.Now().Add(time.Minute*(proxyinabox.VerifyDuration-5)*-1)).Find(&p).Error
+	e = ps.DB.Select("ip,port,id,last_verify").Where("last_verify < ?", time.Now().Add(time.Minute*(proxyinabox.VerifyDuration-5)*-1)).Find(&p).Error
 	return
 }
